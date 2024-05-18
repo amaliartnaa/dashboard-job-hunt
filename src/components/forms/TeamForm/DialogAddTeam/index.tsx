15fabe5716a56aefbf09934674677d4454620da2
amaliartnaa <amaliaratna41@gmail.com>
2024-05-18 16:36:38 +0700

    feat: slicing settings team component 4

--- src/components/forms/TeamForm/DialogAddTeam/index.tsx
+++ src/components/forms/TeamForm/DialogAddTeam/index.tsx
@@ -1,6 +1,12 @@
+"use client"
+
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { teamFormSchema } from '@/lib/form-schema';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { socialMediaFormSchema, teamFormSchema } from '@/lib/form-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { PlusIcon } from 'lucide-react';
 import React, { FC } from 'react'
 import { useForm } from 'react-hook-form';
@@ -9,7 +15,14 @@ import { z } from 'zod';
 interface DialogAddTeamProps {}
 
 const DialogAddTeam: FC<DialogAddTeamProps> = ({ }) => {
-  const form = useForm<z.infer<typeof teamFormSchema>>
+  const form = useForm<z.infer<typeof teamFormSchema>>({
+    resolver: zodResolver(teamFormSchema)
+  })
+
+  const inSubmit = (val: z.infer<typeof teamFormSchema>) => {
+    console.log(val)
+  }
+
   return (
     <div>
       <Dialog>
@@ -21,9 +34,89 @@ const DialogAddTeam: FC<DialogAddTeamProps> = ({ }) => {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle></DialogTitle>
-            <DialogDescription></DialogDescription>
+            <DialogTitle>Add new team</DialogTitle>
+            <DialogDescription>
+              Fill the field to add new team
+            </DialogDescription>
           </DialogHeader>
+
+          <Separator />
+
+          <Form {...form}>
+            <form 
+              onSubmit={form.handleSubmit(inSubmit)}
+              className="space-y-5"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Website</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Name" 
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="position"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Position</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Position" 
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="grid grid-cols-2 gap-3">
+                <FormField
+                  control={form.control}
+                  name="instagram"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Instagram</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Instagram" 
+                          {...field} 
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="linkedin"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Linkedin</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Linkedin" 
+                          {...field} 
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <Button>Save</Button>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
