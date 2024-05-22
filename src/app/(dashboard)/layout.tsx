ba5919b94871c9a5d90d82ae09d0868d0b4d4301
amaliartnaa <amaliaratna41@gmail.com>
2024-05-22 10:36:29 +0700

    feat: integration header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { Epilogue } from "next/font/google";
 import "../globals.css";
 import Sidebar from "@/components/layouts/Sidebar/index";
 import Header from "@/components/layouts/Header";
+import NextAuthProvider from "@/context/NextAuthProvider";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
@@ -20,21 +21,23 @@ export default function RootLayout({
     <html lang="en">
       <body className={epilogue.className}>
         <main>
-          <div className="border-t">
-            <div className="bg-background">
-              <div className="flex flex-row">
-                <div className="hidden lg:block w-[18%]">
-                  <Sidebar />
-                </div>
-                <div className="col-span-3 overflow-auto lg:col-span-5 lg:border-l w-[82%]">
-                  <div className="px-6 py-6 lg:px-8">
-                    <Header />
-                    {children}
+          <NextAuthProvider>
+            <div className="border-t">
+              <div className="bg-background">
+                <div className="flex flex-row">
+                  <div className="hidden lg:block w-[18%]">
+                    <Sidebar />
+                  </div>
+                  <div className="col-span-3 overflow-auto lg:col-span-5 lg:border-l w-[82%]">
+                    <div className="px-6 py-6 lg:px-8">
+                      <Header />
+                      {children}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </NextAuthProvider>
         </main>
       </body>
     </html>
--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { PlusIcon } from 'lucide-react'
+import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React, { FC } from 'react'
 
@@ -10,13 +11,16 @@ interface HeaderProps {}
 const Header: FC<HeaderProps> = ({ }) => {
 
     const router = useRouter()
+    const {data: session} = useSession()
+
+    console.log(session)
 
     const navCreateJobPage = () => router.push('/post-a-job')
     return (
         <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
             <div className="">
                 <div className="">Company</div>
-                <div className="font-semibold">Twitter</div>
+                <div className="font-semibold">{session?.user.name}</div>
             </div>
             <div className="">
                 <Button onClick={navCreateJobPage} className="rounded-none py-3 px-6">
